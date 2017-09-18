f3c0bf10ce0139163d641946389818584fd137ce
Joshua Carp <jm.carp@gmail.com>
2017-09-18 14:02:49 -0400

    Initial commit.

---
+++ ci/compile-broker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eux
+
+export GOPATH=$(pwd)/gopath
+target="$(pwd)/compiled"
+
+pushd gopath/src/code.cloudfoundry.org/nfsbroker
+  go get ./...
+  go build -o bin/nfsbroker
+  cp -r bin "${target}/"
+popd
+
+cp cf-manifests/ci/Procfile compiled
