b537b26e856437f5cb161b35845a1220d8b2e726
Joshua Carp <jm.carp@gmail.com>
2017-09-18 17:51:00 -0400

    Add staging and production environments.

--- ci/compile-broker.sh
+++ ci/compile-broker.sh
@@ -3,10 +3,15 @@
 set -eux
 
 export GOPATH=$(pwd)/gopath
+export PATH=$PATH:$GOPATH/bin
 target="$(pwd)/compiled"
 
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/gomega
+
 pushd gopath/src/code.cloudfoundry.org/nfsbroker
-  go get ./...
+  go get -t ./...
+  ginkgo -r
   go build -o bin/nfsbroker
   cp -r bin "${target}/"
 popd
