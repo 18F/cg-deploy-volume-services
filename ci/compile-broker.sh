53c20408c0fc75c58022751efb40134aab12381e
Joshua Carp <jm.carp@gmail.com>
2017-12-08 12:13:14 -0500

    Bundle dependencies with dep to avoid conflicts.

--- ci/compile-broker.sh
+++ ci/compile-broker.sh
@@ -6,11 +6,12 @@ export GOPATH=$(pwd)/gopath
 export PATH=$PATH:$GOPATH/bin
 target="$(pwd)/compiled"
 
+go get github.com/golang/dep/cmd/dep
 go get github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega
 
 pushd gopath/src/code.cloudfoundry.org/nfsbroker
-  go get -t ./...
+  dep init
   ginkgo -r
   go build -o bin/nfsbroker
   cp -r bin "${target}/"
