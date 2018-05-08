07e182a0506f40b0bb03dd2cfe1065ce5e59c9e4
Joshua Carp <jm.carp@gmail.com>
2018-05-08 15:19:13 -0400

    Build broker from bosh release.

--- ci/compile-broker.sh
+++ ci/compile-broker.sh
@@ -2,16 +2,14 @@
 
 set -eux
 
-export GOPATH=$(pwd)/gopath
+export GOPATH=$(pwd)/release-src
 export PATH=$PATH:$GOPATH/bin
-target="$(pwd)/compiled"
+target=$(pwd)/compiled
 
-go get github.com/golang/dep/cmd/dep
 go get github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega
 
-pushd gopath/src/code.cloudfoundry.org/nfsbroker
-  dep init
+pushd release-src/src/code.cloudfoundry.org/nfsbroker
   ginkgo -r
   go build -o bin/nfsbroker
   cp -r bin "${target}/"
