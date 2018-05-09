95f0d191e413abba1ae519a109d0ace6af06ba64
Joshua Carp <jm.carp@gmail.com>
2018-05-09 15:45:17 -0400

    Fix create and bind options in pora test.

--- ci/acceptance-test.sh
+++ ci/acceptance-test.sh
@@ -10,15 +10,19 @@ pushd release-src/src/code.cloudfoundry.org/persi-acceptance-tests/assets/pora
   cf push --no-start pora
 popd
 
-cf create-service "${SERVICE_NAME:-nfs}" "${PLAN_NAME:-Existing}" "${INSTANCE_NAME:-nfs-volume}" -c <(cat <<EOF
+cf create-service "${SERVICE_NAME:-nfs}" "${PLAN_NAME:-Existing}" "${INSTANCE_NAME:-nfs-volume}" -c "$(cat <<EOF
 {
-  "uid": 1000,
-  "gid": 1000,
   "share": "nfstestserver.service.cf.internal/export/${SHARE_ID}"
 }
 EOF
-)
-cf bind-service pora "${INSTANCE_NAME:-nfs-volume}"
+)"
+cf bind-service pora "${INSTANCE_NAME:-nfs-volume}" -c "$(cat <<EOF
+{
+  "uid": 1000,
+  "gid": 1000
+}
+EOF
+)"
 cf start pora
 
 url=$(cf app pora | grep routes | awk '{print $2}')
