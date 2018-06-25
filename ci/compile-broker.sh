54ebfcee114967f986f82ff9b47de039b2e1720c
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2018-06-25 14:54:34 -0400

    Fix path for default_services

--- ci/compile-broker.sh
+++ ci/compile-broker.sh
@@ -16,4 +16,4 @@ pushd release-src/src/code.cloudfoundry.org/nfsbroker
 popd
 
 cp config/ci/Procfile compiled
-cp config/default_services.json compiled
+cp release-src/src/code.cloudfoundry.org/nfsbroker/default_services.json compiled
