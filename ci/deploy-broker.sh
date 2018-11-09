c191599f56d99834c6b3dc66f4a14bba1f6fff5e
Jon Carmack <jonathan.carmack@gsa.gov>
2018-11-09 12:12:31 -0500

    Add new task to deploy broker manually using bash script

---
+++ ci/deploy-broker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eux
+cat > vars.yml <<- "EOF"
+development-auth-name: ${USERNAME}
+development-auth-pass: ${PASSWORD}
+EOF
+spruce merge vars.yml "${MANIFEST}" > manifest.yml
+
+cf api "${CF_API_URL}"
+(set +x; cf auth "${CF_USERNAME}" "${CF_PASSWORD}")
+cf target -o "${CF_ORGANIZATION}" -s "${CF_SPACE}"
+
+cf zero-downtime-push "${APP-NAME}" -p compiled -m manifest.yml
