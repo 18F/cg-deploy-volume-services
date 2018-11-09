8f016e346001dcddc4b9d400808d678beed9a533
Jon Carmack <jonathan.carmack@gsa.gov>
2018-11-09 15:57:39 -0500

    Install cf-client

--- ci/deploy-broker.sh
+++ ci/deploy-broker.sh
@@ -2,12 +2,14 @@
 
 set -eux
 go get github.com/geofffranks/spruce/...
+curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar xzv -C /usr/local/bin cf && \
+cf install-plugin autopilot -f -r CF-Community
 
 cat > vars.yml <<- "EOF"
 development-auth-name: ${USERNAME}
 development-auth-pass: ${PASSWORD}
 EOF
-spruce merge vars.yml "${MANIFEST}" > manifest.yml
+spruce merge "${MANIFEST}" vars.yml > manifest.yml
 
 cf api "${CF_API_URL}"
 (set +x; cf auth "${CF_USERNAME}" "${CF_PASSWORD}")
